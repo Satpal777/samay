4374037191757b4e7baf8dcdfd013c86c35b9d3b
Satpal777 <rsshacker777@gmail.com>
2026-05-16 09:53:03 +0530

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="bg-slate-950 h-screen w-screen">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
